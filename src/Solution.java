f20ba33e75b6ecfb6732d91a4177aead215dd5be
BadHard <BadHard101@yandex.ru>
2024-01-18 22:28:07 +0300

    new sol (Brian Kernighan Algorithm)

--- src/Solution.java
+++ src/Solution.java
@@ -1,11 +1,14 @@
 public class Solution {
-    // you need to treat n as an unsigned value
+ 
     public int hammingWeight(int n) {
-	int ones = 0;
-    	while(n!=0) {
-    		ones = ones + (n & 1);
-    		n = n>>>1;
-    	}
-    	return ones;
-}
+        
+		int setBitCount = 0;
+        
+		while (n != 0) {
+            n &= (n - 1); // to clear the right most set bit
+            ++setBitCount;
+        }
+		
+        return setBitCount;
+    }
 }
